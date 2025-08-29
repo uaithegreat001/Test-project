ea4e2464ca2249b3e190d6bf8f3c4dce96d00a59
Dev u <uaithegreat001@gmail.com>
2025-08-29 18:33:50 +0100

    update sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -7,38 +7,38 @@ function Sidebar() {
       <nav className='mt-15'>
         <ul className="space-y-5">
           <li>
-            <Link to="/dashboard" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideLayoutDashboard strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Dashboard
+            <Link to="/dashboard" className="flex items-center gap-2 text-gray-700 hover:text-blue-600  text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideLayoutDashboard strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Dashboard
             </Link>
           </li>
           <li>
-            <Link to="/buy-bin" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideArchive strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Buy bin
+            <Link to="/buy-bin" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideArchive strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Buy bin
             </Link>
           </li>
           <li>
-            <Link to="/request-schedule" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideCalendarRange strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Request schedule
+            <Link to="/request-schedule" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideCalendarRange strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Request schedule
             </Link>
           </li>
           <li>
-            <Link to="/payments" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideCreditCard strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Payments
+            <Link to="/payments" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideCreditCard strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Payments
             </Link>
           </li>
           <li>
-            <Link to="/profile" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideUser strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Profile
+            <Link to="/profile" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideUser strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Profile
             </Link>
           </li>
           <li>
-            <Link to="/help" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideHelpCircle strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Help
+            <Link to="/help" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideHelpCircle strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Help
             </Link>
           </li>
           <li>
-            <Link to="/logout" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-base font-medium transition-all duration-300 ease-in-out">
-              <LucideLogOut strokeWidth={2.25} className="h-5 w-5 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Logout
+            <Link to="/logout" className="flex items-center gap-2 text-gray-700 hover:text-blue-600 text-sm font-medium transition-all duration-300 ease-in-out">
+              <LucideLogOut strokeWidth={2.25} className="h-4 w-4 text-gray-700 hover:text-blue-600 font-medium transition-all duration-300 ease-in-out" /> Logout
             </Link>
           </li>
         </ul>
